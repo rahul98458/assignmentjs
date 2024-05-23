2901144e7fa82342735ec91d3359d6edce3859d9
rahul <rahulagr306@gmail.com>
2024-05-23 12:08:44 +0545

    changing redxu color,shape and size

--- react/nextjsdemo/src/app/box/page.js
+++ react/nextjsdemo/src/app/box/page.js
@@ -1,13 +1,25 @@
 'use client'
+import { changeColor, changeHeight, changeShape, changeWidth } from '@/redux/reducerSlices/boxSlice'
+import { Button, Input } from '@nextui-org/react'
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector,useDispatch } from 'react-redux'
+
+
 
 const box = () => {
-    const {backgroundColor,width,height} = useSelector(state=>state.box)
+    const {backgroundColor,width,height,borderRadius} = useSelector(state=>state.box)
+    const dispatch = useDispatch()
   return (
-    <div style={{backgroundColor:backgroundColor , height:height , width : width}}> 
-       
+    <div>
+    <div style={{backgroundColor:backgroundColor , height:height , width : width,borderRadius:borderRadius}}> 
     </div>
+      <Button onClick={()=>dispatch(changeWidth())}>+width</Button>
+       <Button onClick={()=>dispatch(changeHeight())}>+height</Button>
+       <Button onClick={()=>dispatch(changeShape())}>change shape</Button>
+       <Input placeholder='enter the new color' onChange={(e)=> dispatch(changeColor(e.target.value))}/>
+      
+    </div>
+  
   )
 }
 
--- react/nextjsdemo/src/redux/reducerSlices/boxSlice.js
+++ react/nextjsdemo/src/redux/reducerSlices/boxSlice.js
@@ -3,7 +3,8 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = { 
     backgroundColor :'red',
     width : 50 ,
-    height : 50 
+    height : 50 ,
+    borderRadius : '0%'
  }
 
 const boxSlice = createSlice({
@@ -11,11 +12,26 @@ const boxSlice = createSlice({
   initialState,
   reducers: {
     changeHeight(state) {
-        state.height=state.height+1
+        state.height=state.height+10
     },
-   
+    changeWidth(state) {
+      state.width=state.width+10
+  },
+  changeColor(state,actions) {
+    state.backgroundColor=actions.payload
+    },
+    changeShape(state) {
+      if(state.borderRadius=== '0%')
+        {
+      state.borderRadius= '50%'
+        }
+        else
+        {
+          state.borderRadius= '0%'
+        }
+      },
   },
 })
 
-export const { changeHeight } = boxSlice.actions
+export const { changeHeight,changeWidth,changeShape,changeColor } = boxSlice.actions
 export default boxSlice.reducer
